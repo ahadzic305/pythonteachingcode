25777fbe8b0b1fce54e9f7d75c23a55ee03b4f32
ahadzic305 <122333564+ahadzic305@users.noreply.github.com>
2023-02-04 14:28:52 -0500

    Add files via upload
    Required files for module 1

---
+++ Python Absolute Beginner/Create Allergy check code Allen Hadzic.py
@@ -0,0 +1,21 @@
+# Create Allergy check code
+
+# [ ] get input for input_test variable
+
+input_test=input ("Food eaten in last 24 hours?").lower()
+
+# [ ] print "True" message if "dairy" is in the input or False message if not
+
+print ("Dairy eaten = ", "dairy" in input_test)
+
+# [ ] print True message if "nuts" is in the input or False if not
+
+print ("Nuts eaten = ", "nuts" in input_test)
+
+# [ ] Challenge: Check if "seafood" is in the input - print message
+
+print ("Seafood eaten = ", "seafood" in input_test)
+
+# [ ] Challenge: Check if "chocolate" is in the input - print message
+
+print ("Chocolate eaten = ", "chocolate" in input_test)
